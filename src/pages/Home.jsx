9ba376fe4f91b47111643209be5d8fd6f61025d3
Matheus <mf635097@gmail.com>
2022-07-19 08:19:19 -0300

    Feito components e pages do projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+const Home = () => {
+  const [bestMovies, setBestMovies] = useState([]);
+
+  const getBestRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setBestMovies(data.results);
+  };
+
+  useEffect(() => {
+    const bestRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+    getBestRatedMovies(bestRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {bestMovies.length < 0 && <p>Carregando...</p>}
+        {bestMovies.length > 0 &&
+          bestMovies.map((movie) => <p key={movie.id}>{movie.title}</p>)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
