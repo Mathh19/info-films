3ccf7973cdce5a4fdc3db3cc4f7691fed3118b30
Matheus <mf635097@gmail.com>
2022-08-16 18:23:03 -0300

    Criado GoTop para a página

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import GoTop from './components/GoTop/GoTop';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <GoTop />
       <Outlet />
     </div>
   );
---
+++ src/components/GoTop/GoTop.jsx
@@ -0,0 +1,12 @@
+import { ImCtrl } from 'react-icons/im';
+import './GoTop.css';
+
+const GoTop = () => {
+  return (
+    <a className="GoTop" href="#">
+      <ImCtrl />
+    </a>
+  );
+};
+
+export default GoTop;
