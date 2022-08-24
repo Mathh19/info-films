ae13da87ce928c923eaf9d8ffcf954463f52ff97
Matheus <mf635097@gmail.com>
2022-08-24 17:21:16 -0300

    Adicionando pagina de error 404

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import './index.css';
 import Category from './pages/Category/Category';
 import Home from './pages/Home/Home';
 import Movie from './pages/Movie/Movie';
+import PageNotFound from './pages/Page404/Page404';
 import Search from './pages/Search/Search';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
           <Route path="/movie/:id" element={<Movie />} />
           <Route path="search" element={<Search />} />
           <Route path="category" element={<Category />} />
---
+++ src/pages/Page404/Page404.jsx
@@ -0,0 +1,22 @@
+import './Page404.css';
+
+const PageNotFound = () => {
+  return (
+    <div className="container">
+      <div className="page-error">
+        <h1>Error 404</h1>
+        <img
+          src="./page_not_found.svg"
+          alt="Uma imagem de error 404 para página não encontrada"
+        />
+        <p>Página não encontrada</p>
+        <span>:(</span>
+        <p>
+          Você pode voltar para a página inicial clicando <a href="/">aqui</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
