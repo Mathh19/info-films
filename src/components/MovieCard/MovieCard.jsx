3ecd355f17d2718702b49aff7298d4b43426d343
Matheus <mf635097@gmail.com>
2022-12-13 08:23:54 -0300

    Corrigindo validação no título do filme ou série

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -17,10 +17,11 @@ const MovieCard = ({ movie, showLink = true }) => {
         alt={`${movie.media_type === 'movie' ? movie.title : movie.name}`}
         className="img-movie"
       />
-      {movie.media_type === 'tv' && (
+      {movie.media_type === 'tv' ? (
         <h2 className="title-movie">{movie.name}</h2>
+      ) : (
+        <h2 className="title-movie">{movie.title}</h2>
       )}
-      <h2 className="title-movie">{movie.title}</h2>
       <p className="container-star">
         <BsFillStarFill className="star" />{' '}
         {`${movie.vote_average.toFixed(1)} (${movie.vote_count})`}
