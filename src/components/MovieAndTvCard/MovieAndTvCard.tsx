9fe2bcdee7da7a8c24666f68fd2beb50e4452963
Matheus <mf635097@gmail.com>
2022-12-15 17:44:29 -0300

    Melhorando clique na informação do filme para mobile

--- src/components/MovieAndTvCard/MovieAndTvCard.tsx
+++ src/components/MovieAndTvCard/MovieAndTvCard.tsx
@@ -14,24 +14,22 @@ const MovieAndTvCard = ({ movie }: MovieCardProps) => {
 
   return (
     <div className="movie-card">
-      <img
-        src={
-          movie.poster_path === null
-            ? 'https://res.cloudinary.com/ddiiakz1t/image/upload/v1670534777/Logo-InfoFilms/no-image_ultzjy.jpg'
-            : imageUrl + movie.poster_path
-        }
-        alt={`${movie.media_type === 'movie' ? movie.title : movie.name}`}
-        className="img-movie"
-      />
-      {movie.media_type === 'tv' ? (
-        <Link to={`/${movie.media_type}/${movie.id}`}>
+      <Link to={movie.media_type === 'tv' ? `/${movie.media_type}/${movie.id}` : `/movie/${movie.id}`}>
+        <img
+          src={
+            movie.poster_path === null
+              ? 'https://res.cloudinary.com/ddiiakz1t/image/upload/v1670534777/Logo-InfoFilms/no-image_ultzjy.jpg'
+              : imageUrl + movie.poster_path
+          }
+          alt={`${movie.media_type === 'movie' ? movie.title : movie.name}`}
+          className="img-movie"
+        />
+        {movie.media_type === 'tv' ? (
           <h2 className="title-movie">{movie.name}</h2>
-        </Link>
-      ) : (
-        <Link to={`/movie/${movie.id}`}>
+        ) : (
           <h2 className="title-movie">{movie.title}</h2>
-        </Link>
-      )}
+        )}
+      </Link>
       <p className="container-star">
         <BsFillStarFill className="star" />{' '}
         {`${movie.vote_average.toFixed(1)} (${movie.vote_count})`}
