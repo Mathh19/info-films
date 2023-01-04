c68d576f5d05b85add303cd670a4cec0bef95448
Matheus <mf635097@gmail.com>
2023-01-04 17:03:49 -0300

    Mudando loading e retorno de dados não encontrados
    Alterado o estilo do componente loading, corrigido loop de loading quando o usuário buscava por um filme que não existia

--- src/components/MoviesContainer/MoviesContainer.tsx
+++ src/components/MoviesContainer/MoviesContainer.tsx
@@ -5,7 +5,7 @@ import LoadingMovies from '../LoadingMovie/LoadingMovies';
 import MovieAndTvCard from '../MovieAndTvCard/MovieAndTvCard';
 
 import './MoviesContainer.css';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 type MoviesContainerProps = {
   url: string;
@@ -20,7 +20,7 @@ const MoviesContainer = ({ url }: MoviesContainerProps) => {
   const [pages, setPages] = useState(1);
   const [isloading, setIsLoading] = useState(false);
 
-  const getBestMovies = async (url: string) => {
+  const getMovies = async (url: string) => {
     const res = await fetch(url);
     const data = await res.json();
 
@@ -30,17 +30,17 @@ const MoviesContainer = ({ url }: MoviesContainerProps) => {
 
   useEffect(() => {
     const movieUrl = `${url}&page=${page}`;
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 600);
 
-    getBestMovies(movieUrl);
+    getMovies(movieUrl);
   }, [page, query]);
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     event.preventDefault();
     setPage(value);
-    setIsLoading(true)
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 600);
   };
 
   return (
@@ -48,22 +48,29 @@ const MoviesContainer = ({ url }: MoviesContainerProps) => {
       {isloading ? <LoadingMovies /> :
         <>
           <div className="movies-container">
-            {movies.length <= 0 && <LoadingMovies />}
+            {movies.length === 0 &&
+              <div className='movie-not-found'>
+                <p>Sem resultados :(</p>
+                <p>Voltar para o <Link to='/'>inicio</Link></p>
+              </div>
+            }
             {movies.length > 0 &&
               movies.map((movie: MovieProps) => <MovieAndTvCard key={movie.id} movie={movie} />)}
           </div>
-          <div className="container-pagination">
-            <Stack spacing={2}>
-              <Pagination
-                className='pagination'
-                count={pages >= 50 ? 50 : pages}
-                color="primary"
-                variant='outlined'
-                page={page}
-                onChange={handleChange}
-              />
-            </Stack>
-          </div>
+          {movies.length !== 0 &&
+            <div className="container-pagination">
+              <Stack spacing={2}>
+                <Pagination
+                  className='pagination'
+                  count={pages >= 50 ? 50 : pages}
+                  color="primary"
+                  variant='outlined'
+                  page={page}
+                  onChange={handleChange}
+                />
+              </Stack>
+            </div>
+          }
         </>
       }
     </>
