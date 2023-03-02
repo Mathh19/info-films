2cbbf514c551241d10af84d33c2e39ffef31ca4d
Matheus <mf635097@gmail.com>
2023-03-02 17:42:38 -0300

    Refatorando
    Usando o hook criado useEffect melhorando o reuso e requisição para API

--- src/components/MoviesContainer/MoviesContainer.tsx
+++ src/components/MoviesContainer/MoviesContainer.tsx
@@ -1,79 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Pagination, Stack } from '@mui/material';
 import { MovieProps } from '../../shared-types/movie';
-import LoadingMovies from '../LoadingMovie/LoadingMovies';
+import { useFetch } from '../../hooks/useFetch';
+import { DataProps } from '../../shared-types/data';
 import MovieAndTvCard from '../MovieAndTvCard/MovieAndTvCard';
 import Wrapper from '../Wrapper/Wrapper';
+import Loading from '../Loading/Loading';
+import Error from '../Error/Error';
 
 import './MoviesContainer.css';
 
 type MoviesContainerProps = {
   url: string;
-  query?: string | null;
   title: string;
 };
 
-const MoviesContainer = ({ url, title }: MoviesContainerProps) => {
+const MoviesContainer = ({ title, url }: MoviesContainerProps) => {
   const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [pages, setPages] = useState(1);
-  const [isloading, setIsLoading] = useState(false);
-
-  const getMovies = async (url: string) => {
-    try {
-      setIsLoading(true);
-      const res = await fetch(url);
-      const data = await res.json();
-
-      setPages(data.total_pages);
-      setMovies(data.results);
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    const movieUrl = `${url}&page=${page}`;
-    getMovies(movieUrl);
-  }, [page, url]);
+  const { data, isLoading, error } = useFetch<DataProps>(`${url}&page=${page}`);
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     event.preventDefault();
     setPage(value);
   };
 
+  if (isLoading) return <Loading />;
+  if (error) return <Error message={error.message} />;
+
   return (
     <Wrapper>
-      <div className="container">
-        <h2 className="title">
-          <span>{title}</span>
-        </h2>
-        {isloading ? (
-          <LoadingMovies />
-        ) : (
+      {data !== undefined && (
+        <div className="container">
+          <h2 className="title">{title}</h2>
           <div className="movies-container">
-            {movies.length > 0 &&
-              movies.map((movie: MovieProps) => (
-                <MovieAndTvCard key={movie.id} movie={movie} />
-              ))}
+            {data.results.map((movie: MovieProps) => (
+              <MovieAndTvCard key={movie.id} movie={movie} />
+            ))}
           </div>
-        )}
-        {movies.length !== 0 && (
-          <div className="container-pagination">
-            <Stack spacing={2}>
-              <Pagination
-                className="pagination"
-                count={pages >= 50 ? 50 : pages}
-                color="primary"
-                variant="outlined"
-                page={page}
-                onChange={handleChange}
-              />
-            </Stack>
-          </div>
-        )}
-      </div>
+          {data.results.length !== 0 && (
+            <div className="container-pagination">
+              <Stack spacing={2}>
+                <Pagination
+                  className="pagination"
+                  count={data.total_pages >= 50 ? 50 : data.total_pages}
+                  color="primary"
+                  variant="outlined"
+                  page={page}
+                  onChange={handleChange}
+                />
+              </Stack>
+            </div>
+          )}
+        </div>
+      )}
     </Wrapper>
   );
 };
