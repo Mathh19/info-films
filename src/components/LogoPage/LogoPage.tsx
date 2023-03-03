7796d5ed165d5b212479e1dc686c77a61d39e4af
Matheus <mf635097@gmail.com>
2023-03-03 16:45:44 -0300

    Alterando todo o estilo da logo

--- src/components/LogoPage/LogoPage.tsx
+++ src/components/LogoPage/LogoPage.tsx
@@ -3,20 +3,22 @@ import './LogoPage.css';
 
 const LogoPage = () => {
   return (
-    <div className="logo">
-      <h2 className="logo-title">
-        <Link to="/" className="logo-link">
-          <div className="info-logo">
-            <span translate="no">Info</span>
-          </div>
-          <div>
-            <span className="films-logo" translate="no">
-              Films
-            </span>
-          </div>
-        </Link>
-      </h2>
-    </div>
+    <Link to="/" className="logo-link">
+      <div className="logo">
+        <div className="logo-title">
+          <h2 className="logo-info" translate="no">
+            Info
+          </h2>
+          <h2 className="logo-films" translate="no">
+            Films
+          </h2>
+        </div>
+        <img
+          src="./assets/icon-logo-films.svg"
+          alt="Uma imagem de uma fita de filme na logo"
+        />
+      </div>
+    </Link>
   );
 };
 
