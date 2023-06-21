258ffb953a9afb7e4df2965ea521cdd07e74146f
Matheus <mf635097@gmail.com>
2023-06-21 20:55:46 -0300

    Adicionando um novo estilo para a logo do site

--- src/components/LogoPage/LogoPage.tsx
+++ src/components/LogoPage/LogoPage.tsx
@@ -5,18 +5,11 @@ const LogoPage = () => {
   return (
     <Link to="/" className="logo-link">
       <div className="logo">
-        <div className="logo-title">
-          <h2 className="logo-info" translate="no">
-            Info
-          </h2>
-          <h2 className="logo-films" translate="no">
-            FILMS
-          </h2>
-        </div>
         <img
-          src="/assets/icon-logo-films.svg"
+          src="/icon-logo.svg"
           alt="Uma imagem de uma fita de filme na logo"
         />
+        <span>InfoFilms</span>
       </div>
     </Link>
   );
