5dce899469350b01050809134ba9db28550265a8
Matheus <mf635097@gmail.com>
2024-04-23 16:06:02 -0300

    chore: config axios

---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API;
+const token = import.meta.env.VITE_API_TOKEN;
+
+export const api = axios.create({
+  baseURL: `${baseURL}`,
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+});
