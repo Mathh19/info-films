0a543c821c196492fcec9e16c28997897e83f8a4
Matheus <mf635097@gmail.com>
2024-06-12 15:01:51 -0300

    refactor: remove unnecessary style

--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -44,9 +44,7 @@ export const MovieCard = ({
             className="rounded-lg object-cover transition-all group-hover:shadow-md group-hover:shadow-white/20 data-[image='true']:border data-[image='true']:border-white/25 data-[image='true']:bg-gradient-to-br data-[image='true']:from-cyan-600 data-[image='true']:to-transparent data-[image='true']:to-55%"
           />
 
-          <p className="truncate whitespace-nowrap text-xl font-bold">
-            {title}
-          </p>
+          <p className="truncate text-xl font-bold">{title}</p>
         </div>
       </Link>
       <Rating vote_average={vote_average} vote_count={vote_count} />
