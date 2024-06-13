30a54eb77a4569bf9298ccb14b792d1fa03b953c
Matheus <mf635097@gmail.com>
2024-06-13 14:48:00 -0300

    refactor: add message of no information when there was no synopsis

--- src/templates/template-movie-info-page/overview.tsx
+++ src/templates/template-movie-info-page/overview.tsx
@@ -1,3 +1,5 @@
+import { NoInfoMessage } from "../../components/UI/no-info-message";
+
 type OverviewProps = {
   overview: string;
 };
@@ -5,13 +7,15 @@ type OverviewProps = {
 export const Overview = ({ overview }: OverviewProps) => {
   return (
     <>
-      {overview.length > 0 && (
+      <h3 className="mt-28 text-3xl font-bold">Sinopse</h3>
+      {overview.length > 0 ? (
         <div>
-          <h3 className="mt-28 text-3xl font-bold">Sinopse</h3>
           <p className="max-h-64 max-w-2xl overflow-y-auto font-semibold">
             {overview}
           </p>
         </div>
+      ) : (
+        <NoInfoMessage />
       )}
     </>
   );
