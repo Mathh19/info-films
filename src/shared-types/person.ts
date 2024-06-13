2d7b0ca5d40c4d7cf1d296ffc85ebdc5afe2b227
Matheus <mf635097@gmail.com>
2024-06-13 14:21:24 -0300

    feat: add person type

--- src/shared-types/person.ts
+++ src/shared-types/person.ts
@@ -1,10 +1,30 @@
+export type Person = {
+  name: string;
+  also_known_as: string[];
+  biography: string;
+  birthday: string | null;
+  deathday: string | null;
+  profile_path: string | null;
+  place_of_birth: string | null;
+}
+
 export type Cast = {
   id: number;
   name: string;
   character: string;
   profile_path: string;
+  release_date: string;
+  first_air_date: string;
+  title: string;
+  media_type: 'movie' | 'tv';
 }
 
 export type Crew = {
   job: string;
+  departament: string;
 } & Omit<Cast, 'character'>;
+
+export type PersonCredits = {
+  cast: Cast[]
+  crew: Crew[]
+}
