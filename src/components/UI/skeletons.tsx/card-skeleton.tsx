d8397862d093f20f98e750b579153b18474a6393
Matheus <mf635097@gmail.com>
2024-08-20 17:08:59 -0300

    refactor: fix card skeleton style

--- src/components/UI/skeletons.tsx/card-skeleton.tsx
+++ src/components/UI/skeletons.tsx/card-skeleton.tsx
@@ -1,6 +1,6 @@
 export const CardSkeleton = () => {
   return (
-    <div className="animate-pulse space-y-2">
+    <div className="w-60 shrink-0 animate-pulse space-y-2">
       <div className="w-full rounded bg-gradient-to-tr from-slate-700 to-slate-800 py-44"></div>
       <div className="h-2 w-full rounded bg-gradient-to-tr from-slate-700 to-slate-800"></div>
       <div className="h-2 w-full max-w-32 rounded bg-gradient-to-tr from-slate-700 to-slate-800"></div>
