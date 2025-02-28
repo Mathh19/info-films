b605620fb6cdbe8835ae0bd9885a21f4ee2bf6ac
Matheus <mf635097@gmail.com>
2025-02-28 10:55:51 -0300

    fix: remove useTouchScroll

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useHorizontalScroll } from "../hooks/useHorizontalScroll";
-import { useTouchScroll } from "../hooks/useTouchScroll";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
   const { containerRef, handleMouseEnter, handleMouseLeave } =
     useHorizontalScroll();
-  useTouchScroll(containerRef);
 
   const scrollAmount = 300;
 
@@ -28,14 +26,18 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
   return (
     <div className="relative flex w-full items-center">
-      <button
-        onClick={handleScrollLeft}
-        className="absolute z-[1] hidden cursor-pointer rounded-full border border-cyan-400 bg-black/50 p-2 text-cyan-400 drop-shadow transition-all duration-300 hover:bg-black/90 focus:bg-black/90 max-lg:block"
-      >
-        <ArrowLeft />
-      </button>
+      {isMobile && (
+        <button
+          onClick={handleScrollLeft}
+          className="absolute z-[1] cursor-pointer rounded-full border border-cyan-400 bg-black/50 p-2 text-cyan-400 drop-shadow transition-all duration-300 hover:bg-black/90 focus:bg-black/90"
+        >
+          <ArrowLeft />
+        </button>
+      )}
       <div
         ref={containerRef}
         onMouseEnter={handleMouseEnter}
@@ -44,12 +46,14 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
       >
         {children}
       </div>
-      <button
-        onClick={handleScrollRight}
-        className="absolute right-0 z-[1] hidden cursor-pointer rounded-full border border-cyan-400 bg-black/50 p-2 text-cyan-400 drop-shadow transition-all duration-300 hover:bg-black/90 focus:bg-black/90 max-lg:block"
-      >
-        <ArrowRight />
-      </button>
+      {isMobile && (
+        <button
+          onClick={handleScrollRight}
+          className="absolute right-0 z-[1] cursor-pointer rounded-full border border-cyan-400 bg-black/50 p-2 text-cyan-400 drop-shadow transition-all duration-300 hover:bg-black/90 focus:bg-black/90"
+        >
+          <ArrowRight />
+        </button>
+      )}
     </div>
   );
 };
